9d39e6ffea805adf84c1cb4e86f00b0673f013dc
m256i <lespaul05cpt.ne@gmail.com>
2024-02-08 12:45:42 +0100

    asdasdasdxX

--- crypto.h
+++ crypto.h
@@ -1,203 +1,274 @@
 #pragma once
 
-#include "gmp-6.3.0/gmpxx.h"
+#include "gmpxx.h"
 
 namespace blue_crypto
 {
 
-class GmpWrapper {
+class GmpWrapper
+{
 public:
-    // Constructors
-    GmpWrapper() {
-        mpz_init(value_);
+  // Constructors
+  GmpWrapper() { mpz_init(value_); }
+
+  GmpWrapper(const char* str)
+  {
+    if (str[0] == '0' && str[1] == 'x')
+    {
+      mpz_init_set_str(value_, str + 2, 16);
+    }
+    else
+    {
+      mpz_init_set_str(value_, str, 10);
+    }
+  }
+
+  GmpWrapper(const GmpWrapper& other) { mpz_init_set(value_, other.value_); }
+  GmpWrapper(int intValue) { mpz_init_set_si(value_, intValue); }
+
+  // Destructor
+  ~GmpWrapper() { mpz_clear(value_); }
+
+  // Assignment operator
+  GmpWrapper&
+  operator=(const GmpWrapper& other)
+  {
+    if (this != &other)
+    {
+      mpz_set(value_, other.value_);
+    }
+    return *this;
+  }
+
+  // Arithmetic operators
+  GmpWrapper
+  operator+(const GmpWrapper& other) const
+  {
+    GmpWrapper result;
+    mpz_add(result.value_, value_, other.value_);
+    return result;
+  }
+
+  GmpWrapper
+  operator-(const GmpWrapper& other) const
+  {
+    GmpWrapper result;
+    mpz_sub(result.value_, value_, other.value_);
+    return result;
+  }
+
+  GmpWrapper
+  operator*(const GmpWrapper& other) const
+  {
+    GmpWrapper result;
+    mpz_mul(result.value_, value_, other.value_);
+    return result;
+  }
+
+  GmpWrapper
+  operator%(const GmpWrapper& other) const
+  {
+    GmpWrapper result;
+    mpz_mod(result.value_, value_, other.value_);
+    return result;
+  }
+
+  GmpWrapper
+  operator/(const GmpWrapper& other) const
+  {
+    if (mpz_sgn(other.value_) == 0)
+    {
+      // Handle division by zero
+      throw std::invalid_argument("Division by zero");
     }
 
-    GmpWrapper(const char* str) {
-        mpz_init_set_str(value_, str, 10);
+    GmpWrapper result;
+    mpz_tdiv_q(result.value_, value_, other.value_);
+    return result;
+  }
+
+  // Bitwise AND operator
+  GmpWrapper
+  operator&(const GmpWrapper& other) const
+  {
+    GmpWrapper result;
+    mpz_and(result.value_, value_, other.value_);
+    return result;
+  }
+
+  // Bitwise OR operator
+  GmpWrapper
+  operator|(const GmpWrapper& other) const
+  {
+    GmpWrapper result;
+    mpz_ior(result.value_, value_, other.value_);
+    return result;
+  }
+
+  // Bitwise XOR operator
+  GmpWrapper
+  operator^(const GmpWrapper& other) const
+  {
+    GmpWrapper result;
+    mpz_xor(result.value_, value_, other.value_);
+    return result;
+  }
+
+  // Bitwise NOT operator
+  GmpWrapper
+  operator~() const
+  {
+    GmpWrapper result;
+    mpz_com(result.value_, value_);
+    return result;
+  }
+
+  GmpWrapper
+  pow(unsigned int exp) const
+  {
+    GmpWrapper result;
+    mpz_pow_ui(result.value_, value_, exp);
+    return result;
+  }
+
+  unsigned char
+  get_bit(size_t bitIndex) const
+  {
+    return mpz_tstbit(value_, bitIndex);
+  }
+
+  size_t
+  count_trailing_zeros() const
+  {
+    return static_cast<size_t>(mpz_scan1(value_, 0));
+  }
+
+  size_t
+  bitlength() const
+  {
+    return static_cast<size_t>(mpz_sizeinbase(value_, 2));
+  }
+
+  void
+  write() const
+  {
+    char* str = mpz_get_str(nullptr, 10, value_);
+    std::cout << str;
+    free(str);
+  }
+
+  // Equality operator
+  bool
+  operator==(const GmpWrapper& other) const
+  {
+    return mpz_cmp(value_, other.value_) == 0;
+  }
+
+  // Inequality operator
+  bool
+  operator!=(const GmpWrapper& other) const
+  {
+    return mpz_cmp(value_, other.value_) != 0;
+  }
+
+  // Less than operator
+  bool
+  operator<(const GmpWrapper& other) const
+  {
+    return mpz_cmp(value_, other.value_) < 0;
+  }
+
+  // Less than or equal to operator
+  bool
+  operator<=(const GmpWrapper& other) const
+  {
+    return mpz_cmp(value_, other.value_) <= 0;
+  }
+
+  // Greater than operator
+  bool
+  operator>(const GmpWrapper& other) const
+  {
+    return mpz_cmp(value_, other.value_) > 0;
+  }
+
+  // Greater than or equal to operator
+  bool
+  operator>=(const GmpWrapper& other) const
+  {
+    return mpz_cmp(value_, other.value_) >= 0;
+  }
+
+  // Addition with int
+  GmpWrapper
+  operator+(int intValue) const
+  {
+    GmpWrapper n(intValue);
+    return this->operator+(n);
+  }
+
+  // Subtraction with int
+  GmpWrapper
+  operator-(int intValue) const
+  {
+    GmpWrapper n(intValue);
+    return this->operator-(n);
+  }
+
+  GmpWrapper
+  operator-() const
+  {
+    GmpWrapper result;
+    mpz_neg(result.value_, value_);
+    return result;
+  }
+
+  // Multiplication with int
+  GmpWrapper
+  operator*(int intValue) const
+  {
+    GmpWrapper result;
+    mpz_mul_si(result.value_, value_, intValue);
+    return result;
+  }
+
+  // Division with int
+  GmpWrapper
+  operator/(int intValue) const
+  {
+    if (intValue == 0)
+    {
+      // Handle division by zero
+      throw std::invalid_argument("Division by zero");
     }
 
-    GmpWrapper(const GmpWrapper& other) {
-        mpz_init_set(value_, other.value_);
-    }
+    GmpWrapper n(intValue);
+    return this->operator/(n);
+  }
 
-    GmpWrapper(int intValue) {
-        mpz_init_set_si(value_, intValue);
-    }
+  // Output operator
+  friend std::ostream&
+  operator<<(std::ostream& os, const GmpWrapper& gmp)
+  {
+    return os << mpz_get_str(nullptr, 10, gmp.value_);
+  }
 
-    // Destructor
-    ~GmpWrapper() {
-        mpz_clear(value_);
-    }
+  friend GmpWrapper
+  operator*(int lhs, const GmpWrapper& gmp)
+  {
+    GmpWrapper n(lhs);
+    return n * gmp;
+  }
 
-    // Assignment operator
-    GmpWrapper& operator=(const GmpWrapper& other) {
-        if (this != &other) {
-            mpz_set(value_, other.value_);
-        }
-        return *this;
-    }
-
-    // Arithmetic operators
-    GmpWrapper operator+(const GmpWrapper& other) const {
-        GmpWrapper result;
-        mpz_add(result.value_, value_, other.value_);
-        return result;
-    }
-
-    GmpWrapper operator-(const GmpWrapper& other) const {
-        GmpWrapper result;
-        mpz_sub(result.value_, value_, other.value_);
-        return result;
-    }
-
-    GmpWrapper operator*(const GmpWrapper& other) const {
-        GmpWrapper result;
-        mpz_mul(result.value_, value_, other.value_);
-        return result;
-    }
-
-    GmpWrapper operator%(const GmpWrapper& other) const {
-        GmpWrapper result;
-        mpz_mod(result.value_, value_, other.value_);
-        return result;
-    }
-
-    GmpWrapper operator/(const GmpWrapper& other) const {
-        if (mpz_sgn(other.value_) == 0) {
-            // Handle division by zero
-            throw std::invalid_argument("Division by zero");
-        }
-
-        GmpWrapper result;
-        mpz_tdiv_q(result.value_, value_, other.value_);
-        return result;
-    }
-
-        // Bitwise AND operator
-    GmpWrapper operator&(const GmpWrapper& other) const {
-        GmpWrapper result;
-        mpz_and(result.value_, value_, other.value_);
-        return result;
-    }
-
-    // Bitwise OR operator
-    GmpWrapper operator|(const GmpWrapper& other) const {
-        GmpWrapper result;
-        mpz_ior(result.value_, value_, other.value_);
-        return result;
-    }
-
-    // Bitwise XOR operator
-    GmpWrapper operator^(const GmpWrapper& other) const {
-        GmpWrapper result;
-        mpz_xor(result.value_, value_, other.value_);
-        return result;
-    }
-
-    // Bitwise NOT operator
-    GmpWrapper operator~() const {
-        GmpWrapper result;
-        mpz_com(result.value_, value_);
-        return result;
-    }
-
-    GmpWrapper pow(unsigned int exp) const {
-        GmpWrapper result;
-        mpz_pow_ui(result.value_, value_, exp);
-        return result;
-    }
-  
-    unsigned char get_bit(size_t bitIndex) const {
-        return mpz_tstbit(value_, bitIndex);
-    }
-
-    size_t count_trailing_zeros() const {
-        return static_cast<size_t>(mpz_scan1(value_, 0));
-    }
-
-    size_t bitlength() const {
-        return static_cast<size_t>(mpz_sizeinbase(value_, 2));
-    }
-
-    void write() const {
-        char* str = mpz_get_str(nullptr, 10, value_);
-        std::cout << str;
-        free(str);
-    }
-
-        // Equality operator
-    bool operator==(const GmpWrapper& other) const {
-        return mpz_cmp(value_, other.value_) == 0;
-    }
-
-    // Inequality operator
-    bool operator!=(const GmpWrapper& other) const {
-        return mpz_cmp(value_, other.value_) != 0;
-    }
-
-    // Less than operator
-    bool operator<(const GmpWrapper& other) const {
-        return mpz_cmp(value_, other.value_) < 0;
-    }
-
-    // Less than or equal to operator
-    bool operator<=(const GmpWrapper& other) const {
-        return mpz_cmp(value_, other.value_) <= 0;
-    }
-
-    // Greater than operator
-    bool operator>(const GmpWrapper& other) const {
-        return mpz_cmp(value_, other.value_) > 0;
-    }
-
-    // Greater than or equal to operator
-    bool operator>=(const GmpWrapper& other) const {
-        return mpz_cmp(value_, other.value_) >= 0;
-    }
-
-        // Addition with int
-    GmpWrapper operator+(int intValue) const {
-        GmpWrapper n(intValue);
-        return this->operator+(n);
-    }
-
-    // Subtraction with int
-    GmpWrapper operator-(int intValue) const {
-        GmpWrapper n(intValue);
-        return this->operator-(n);
-    }
-
-    GmpWrapper operator-() const {
-        GmpWrapper result;
-        mpz_neg(result.value_, value_);
-        return result;
-    }
-
-    // Multiplication with int
-    GmpWrapper operator*(int intValue) const {
-        GmpWrapper result;
-        mpz_mul_si(result.value_, value_, intValue);
-        return result;
-    }
-
-    // Division with int
-    GmpWrapper operator/(int intValue) const {
-        if (intValue == 0) {
-            // Handle division by zero
-            throw std::invalid_argument("Division by zero");
-        }
-
-        GmpWrapper n(intValue);
-        return this->operator/(n);
-    }
-
-    // Output operator
-    friend std::ostream& operator<<(std::ostream& os, const GmpWrapper& gmp) {
-        return os << mpz_get_str(nullptr, 10, gmp.value_);
-    }
+  friend GmpWrapper
+  operator+(int lhs, const GmpWrapper& gmp)
+  {
+    GmpWrapper n(lhs);
+    return n + gmp;
+  }
 
 private:
-    mpz_t value_;
+  mpz_t value_;
 };
 
-}
+} // namespace blue_crypto
--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
-//#include "bigint.hpp"
+// #include "bigint.hpp"
 
 #include <assert.h>
 #include <exception>
 #include <iostream>
 #include <stdlib.h>
+#include <chrono>
 
 #include "crypto.h"
 
 using namespace blue_crypto;
 
-
 using ix = GmpWrapper;
 
+struct perf_
+{
+  perf_(std::string_view _name) : name(_name) { start = std::chrono::high_resolution_clock::now(); }
+
+  ~perf_()
+  {
+    auto stop     = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << name << ": " << duration.count() << " microseconds" << std::endl;
+  }
+
+  std::string_view name;
+  decltype(std::chrono::high_resolution_clock::now()) start;
+};
 
 ix
 ix_abs(const ix& _n)
@@ -23,44 +37,12 @@ ix_abs(const ix& _n)
 mod(const ix& _na, const ix& _nb) noexcept
 {
   ix result = _na % _nb;
-
-  // if (result < ix{0}) [[unlikely]]
-  // {
-  //   return result + _nb;
-  // }
   return result;
 }
 
 [[gnu::pure]] std::pair<ix, ix>
 divmod(const ix& _na, const ix& _nb) noexcept
 {
-  // if (_na > 0 && _nb > 0)
-  // {
-  //   return {_na / _nb, _na - (_na / _nb) * _nb};
-  // }
-  // if (_na > 0 && _nb < 0)
-  // {
-  //   const ix abs_dist = ix_abs(_nb - _na) - 1;
-  //   const ix div      = (abs_dist / _nb) * -1;
-  //   const ix rem      = ix_abs(_na - (div * _nb * -1));
-
-  //   return {div * -1, rem * -1};
-  // }
-  // if (_na < 0 && _nb > 0)
-  // {
-  //   const ix abs_dist = ix_abs(_nb - _na) - 1;
-  //   const ix div      = (abs_dist / _nb);
-  //   const ix rem      = ix_abs((_na * -1) - (div * _nb));
-
-  //   return {div * -1, rem};
-  // }
-  // if (_na < 0 && _nb < 0)
-  // {
-  //   return {_na / _nb, ((-_na) - ((-_na) / (-_nb)) * (-_nb)) * -1};
-  // }
-
-  // return {0, 0};
-
   return {_na / _nb, mod(_na, _nb)};
 }
 
@@ -120,23 +102,91 @@ struct crv_p
   }
 };
 
+struct jcbn_crv_p
+{
+  ix x{}, y{}, z{};
+
+  void
+  print() const
+  {
+    std::cout << "[";
+    x.write();
+    std::cout << ", ";
+    y.write();
+    std::cout << ", ";
+    z.write();
+    std::cout << "]\n";
+  }
+};
+
+jcbn_crv_p
+to_jacobian(const crv_p& _ws_point)
+{
+  return {_ws_point.x, _ws_point.y, ix{1}};
+}
+
+crv_p
+from_jacobian(const jcbn_crv_p& _jcbn, const ix& _p)
+{
+  ix inv = modinv(_jcbn.z, _p);
+  return {mod(_jcbn.x * inv.pow(2), _p), mod(_jcbn.y * inv.pow(3), _p)};
+}
+
+/* NOTE when using curves where a != 0 this needs to be changed */
+
+jcbn_crv_p
+point_double(const jcbn_crv_p& _p1, const ix& _p)
+{
+  const ix a = mod((_p1.x * 4) * _p1.y.pow(2), _p);
+  const ix b = mod(_p1.x.pow(2) * 3, _p);
+
+  jcbn_crv_p out;
+
+  out.x = mod((a * -2) + b.pow(2), _p);
+  out.y = mod((_p1.y.pow(4) * -8) + b * (a - out.x), _p);
+  out.z = mod(_p1.y * _p1.z * 2, _p);
+
+  return out;
+}
+
+jcbn_crv_p
+point_add(const jcbn_crv_p& _p1, const jcbn_crv_p& _p2, const ix& _p)
+{
+  const ix a = mod(_p1.x * _p1.z.pow(2), _p);
+  const ix b = mod(_p2.x * _p1.z.pow(2), _p);
+  const ix c = mod(_p1.y * _p2.z.pow(3), _p);
+  const ix d = mod(_p2.y * _p1.z.pow(3), _p);
+
+  const ix e = mod(b - a, _p);
+  const ix f = mod(d - c, _p);
+
+  jcbn_crv_p out;
+
+  // out.x = mod(-(e.pow(3)) - (e.pow(2) * a * 2) + f, _p);
+  out.x = (-e.pow(3) - 2 * a * e.pow(2) + f) % _p;
+  out.y = -c * e.pow(3) + f * (a * e.pow(2) - out.x);
+  out.z = (_p1.z * _p2.z * e) % _p;
+
+  return out;
+}
+
 [[gnu::pure]] inline crv_p
-point_add(const crv_p& _p1, const crv_p& _p2, const ix& _p /* global modulo */) noexcept
+point_add(const crv_p& _p1, const crv_p& _p2, const ix& _p) noexcept
 {
   const ix s = mod((_p2.y - _p1.y) * modinv(_p2.x - _p1.x, _p), _p);
   ix x3      = mod((s.pow(2) - _p1.x - _p2.x), _p);
   ix y3      = mod(s * (_p1.x - x3) - _p1.y, _p);
-
   return {x3, y3};
 }
 
 [[gnu::pure]] inline crv_p
 point_double(const crv_p& _p1, const ix& _p) noexcept
 {
-  const ix x2 = _p1.x;
-  ix s        = mod(((ix{3} * _p1.x.pow(2)) * modinv(ix{3} * _p1.y, _p)), _p);
-  ix x3       = mod((s.pow(2) - _p1.x - x2), _p);
-  ix y3       = mod((s * (_p1.x - x3) - _p1.y), _p);
+  const ix& x2 = _p1.x;
+
+  ix s  = mod(((ix{3} * _p1.x.pow(2)) * modinv(ix{2} * _p1.y, _p)), _p);
+  ix x3 = mod((s.pow(2) - _p1.x - x2), _p);
+  ix y3 = mod((s * (_p1.x - x3) - _p1.y), _p);
 
   return {x3, y3};
 }
@@ -183,6 +233,30 @@ double_and_add(const crv_p& _p1, const ix& _num, const ix& _p)
   return _p3;
 }
 
+jcbn_crv_p
+double_and_add(const jcbn_crv_p& _p1, const ix& _num, const ix& _p)
+{
+  const std::size_t bits = bits_to_represent(_num);
+  const jcbn_crv_p _p2   = _p1;
+  jcbn_crv_p _p3         = _p1;
+
+  for (std::size_t i = 2; i != bits + 1; ++i)
+  {
+    if (is_bit_set(_num, bits - i))
+    {
+      _p3 = point_double(_p3, _p);
+      _p3 = point_add(_p2, _p3, _p);
+    }
+    else
+    {
+
+      _p3 = point_double(_p3, _p);
+    }
+  }
+
+  return _p3;
+}
+
 /*
   BIG DISCLAIMER: UNFINISHED AND NOT TESTED!!!
 
@@ -195,27 +269,46 @@ double_and_add(const crv_p& _p1, const ix& _num, const ix& _p)
 int
 main()
 {
-  ix mod_global = "115792089237316195423570985008687907853269984665640564039457584007908834671663";
+  ix mod_global = "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f";
 
-  crv_p G = {"55066263022277343669578718895168534326250603453777594175500187360389116729240",
-             "32670510020758816978083085130507043184471273380659243275938904335757337482424"};
+  crv_p G = {"0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+             "0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8"};
 
   ix privKeyA = "40505654708211189456746820883201845994248137211058198699828051064905928553035";
   ix privKeyB = "83862260130769358743610306176715755043868098730045613807339143668249321773381";
 
-  crv_p pubKeyA = double_and_add(G, privKeyA, mod_global);
-  pubKeyA.print();
+  auto p2G = point_double(G, mod_global);
 
-  crv_p pubKeyB = double_and_add(G, privKeyB, mod_global);
-  pubKeyB.print();
+  std::cout << "singular double of G: ";
+  {
+    perf_ _("affine");
 
-  crv_p shared_secretA = double_and_add(pubKeyB, privKeyA, mod_global);
-  crv_p shared_secretB = double_and_add(pubKeyA, privKeyB, mod_global);
+    // point_double(point_double(G, mod_global), mod_global).print();
+    point_add(G, p2G, mod_global).print();
+  }
 
-  std::cout << "shared secrets: \n";
+  std::cout << "jacobian: ";
+  {
+    perf_ _("jacobian");
+    // from_jacobian(point_double(point_double(to_jacobian(G), mod_global), mod_global), mod_global).print();
+    from_jacobian(point_add(to_jacobian(G), to_jacobian(p2G), mod_global), mod_global).print();
+  }
 
-  shared_secretA.print();
-  shared_secretB.print();
+  // crv_p pubKeyA = double_and_add(G, privKeyA, mod_global);
+  // pubKeyA.print();
+  // crv_p pubKeyB = double_and_add(G, privKeyB, mod_global);
+  // pubKeyB.print();
+  // jcbn_crv_p pubKeyAJ = to_jacobian(pubKeyA);
+  // jcbn_crv_p pubKeyBJ = to_jacobian(pubKeyB);
+  // jcbn_crv_p shared_secretAJ = double_and_add(pubKeyBJ, privKeyA, mod_global);
+  // jcbn_crv_p shared_secretBJ = double_and_add(pubKeyAJ, privKeyB, mod_global);
+  // crv_p shared_secretA = double_and_add(pubKeyB, privKeyA, mod_global);
+  // crv_p shared_secretB = double_and_add(pubKeyA, privKeyB, mod_global);
+  // std::cout << "shared secrets: \n";
+  // shared_secretA.print();
+  // shared_secretB.print();
+  // from_jacobian(shared_secretAJ, mod_global).print();
+  // from_jacobian(shared_secretBJ, mod_global).print();
 
   return 0;
 }
