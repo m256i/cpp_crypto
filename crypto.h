118a7bee191c04284861c6c8ca306216c24b35fd
m256i <lespaul05cpt.ne@gmail.com>
2024-03-21 03:45:25 +0100

    blablalba final commit before movement away

--- crypto.h
+++ crypto.h
@@ -7,6 +7,8 @@
 #include <cstddef>
 #include <iostream>
 
+// TODO: possible performance improvements can be done in this file
+
 namespace blue_crypto
 {
 
@@ -158,29 +160,6 @@ public:
     }
 
     return out;
-  }
-
-  std::size_t
-  get_bits2(std::size_t start_bit, std::size_t num_bits) const
-  {
-    std::string bin_rep{mpz_get_str(nullptr, 2, value_)};
-    
-   // std::cout << "get_bits2() : bin_rep = " << bin_rep << "\n";
-
-    std::reverse(bin_rep.begin(), bin_rep.end());
-
-    //std::cout << "get_bits2() : bin_rep reversed = " << bin_rep << "\n";
-    
-
-    std::string cut = bin_rep.substr(start_bit, num_bits);
-
-   // std::cout << "get_bits2() : cut = " << cut << "\n";
-    
-    
-    std::reverse(cut.begin(), cut.end());
-    //std::cout << "get_bits2() : cut reverse = " << cut << "\n";
-
-    return std::stoi(cut, nullptr, 2);
   }
 
   size_t
